95ef468dfceb443279aab661497681f55c284d5a
Nazareno Gutierrez <nazarenojunin@gmail.com>
2024-04-29 18:58:08 -0300

    SEO changes

--- src/app/sections/section-como-comprar/index.tsx
+++ src/app/sections/section-como-comprar/index.tsx
@@ -21,31 +21,31 @@ const SectionComoComprar = () => {
         </h2>
         <div className="grid grid-cols-3 text-4xl mt-5">
           <div className="place-self-center pl-36 mt-5">
-            <h2 className={`${highlightBLue.fancyNoHover} w-fit`}>
+            <h3 className={`${highlightBLue.fancyNoHover} w-fit`}>
               Mirá nuestro catálogo
-            </h2>
+            </h3>
           </div>
           <Image src={arrow1blue} className="mb-16 arrow-animation pointer-events-none" alt="Como comprar" width={500} height={0}></Image> 
           <div className="pl-8 mt-2">
-            <GoToButton sectionId="#nuestros-productos" classname="w-fit" highlightColor="green">
-              Elegí tu nordicap
+            <GoToButton sectionId="#nuestros-productos" classname="w-fit hover:text-[#71C417] group transition" highlightColor="green">
+              <h3>Elegí tu nordicap</h3>
             </GoToButton>
           </div>
           <Image src={arrow2blue} className="rotate-[25deg] place-self-end mt-20 -m-16 -scale-x-100 arrow-animation pointer-events-none" alt="Como comprar" width={200} height={0}></Image>
-          <div className="pl-16 place-self-center">
-            <GoToButton sectionId="#nosotros" highlightColor="blue">
-              Contactanos para
+          <div className="pl-16 place-self-center group">
+            <GoToButton classname="group-hover:text-[#62A9D7] transition" sectionId="#nosotros" highlightColor="blue">
+              <span>Contactanos para</span>
               <span className="mx-1" ></span>
             </GoToButton>
-            <GoToButton sectionId="#nosotros" highlightColor="green">
-                realizar tu pedido
+            <GoToButton classname="group-hover:text-[#71C417] transition" sectionId="#nosotros" highlightColor="green">
+              <h3>realizar tu pedido</h3>
             </GoToButton>
           </div>
           <Image src={arrow3green} className="-scale-x-100 rotate-[210deg] -mt-28 ml-24 arrow-animation pointer-events-none" alt="Como comprar" width={230} height={0}></Image>
           <div className="pl-16 col-span-2 place-self-center mt-32">
-            <h2 className={`${highlightBLue.fancyNoHover}`}>
+            <h3 className={`${highlightBLue.fancyNoHover}`}>
               Y a disfrutarla!
-            </h2>
+            </h3>
           </div>
         </div>
       </div>
