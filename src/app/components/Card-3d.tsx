32417ec8b1255680168f1b7711554c7c18844580
Nazareno Gutierrez <nazarenojunin@gmail.com>
2024-06-07 20:55:25 -0300

    Added instagram link

--- src/app/components/Card-3d.tsx
+++ src/app/components/Card-3d.tsx
@@ -5,27 +5,30 @@ import Atropos from "atropos/react";
 import "atropos/css";
 import Image from "next/image";
 import iphoneImg from "../../assets/iphone/iphone-view2.avif";
+import Link from "next/link";
 
 export default function Card3d() {
   return (
     <div className="pr-36">
-      <Atropos
-        shadow={true}
-        highlight={false}
-        shadowOffset={150}
-        rotateXMax={10}
-        rotateYMax={10}
-        activeOffset={15}
-        className="my-atropos shadow-neutral-100 flex items-center justify-center"
-      >
-        <Image
-          src={iphoneImg}
-          data-atropos-offset="-5"
-          alt="vista desde un iphone"
-          width={300}
-          height={300}
-        />
-      </Atropos>
+      <Link href={"https://www.instagram.com/nordicapsjunin/"} target="_blank">
+        <Atropos
+          shadow={true}
+          highlight={false}
+          shadowOffset={150}
+          rotateXMax={20}
+          rotateYMax={20}
+          activeOffset={15}
+          className="my-atropos shadow-neutral-100 flex items-center justify-center"
+        >
+          <Image
+            src={iphoneImg}
+            data-atropos-offset="-5"
+            alt="vista desde un iphone"
+            width={300}
+            height={300}
+          />
+        </Atropos>      
+      </Link>
     </div>
   );
 }
